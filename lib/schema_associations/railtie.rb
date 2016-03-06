9bd76bf0250394da25d509bfa9b1a570625a6040
ronen barzel <ronen@barzel.org>
2016-03-05 22:43:23 -0500

    Cleanup: Use schema_monkey rather than explicitly patching ActiveRecord.
    This eliminates the various alias_method_chains, and the railtie.
    
    Addresses #16

--- lib/schema_associations/railtie.rb
+++
@@ -1,9 +0,0 @@
-module SchemaAssociations
-  class Railtie < Rails::Railtie #:nodoc:
-
-    initializer 'schema_associations.insert', :after => :load_config_initializers do
-      SchemaAssociations.insert
-    end
-
-  end
-end
